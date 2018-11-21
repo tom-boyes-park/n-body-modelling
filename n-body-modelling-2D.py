535975b72592bc6662c053adc20cb1cb972d902f
Tom Boyes-Park <tombp23@googlemail.com>
2018-11-21 12:16:42 +0000

    Created function to write orbit data to csv

--- n-body-modelling-2D.py
+++ n-body-modelling-2D.py
@@ -21,8 +21,7 @@ class Body:
 def set_initial(bodies):
     initial = []
 
-    # Loop through bodies and create inital conditions to be passed
-    # into the integrator below
+    # Loop through bodies and create initial conditions to be passed into the integrator
     for body in bodies:
         values = [body.x, body.vx, body.y, body.vy]
         initial += values
@@ -37,15 +36,14 @@ def n_body_func(t, pos_vel_array, bodies):
     # Set up array to store updated spatial and velocity values
     dpos_vel_array = np.zeros(4 * len(bodies))
 
-    # Arbritrary value for G (gravitational constant)
+    # Arbitrary value for G (gravitational constant)
     G = 1
 
     # Change in x, y is velocity in x, y
     dpos_vel_array[0:len(dpos_vel_array):4] = pos_vel_array[1:len(pos_vel_array):4]
     dpos_vel_array[2:len(dpos_vel_array):4] = pos_vel_array[3:len(pos_vel_array):4]
 
-    # Loop through bodies, calculating change in vx, vy
-    # due to all other bodies
+    # Loop through bodies, calculating change in vx, vy due to all other bodies
     for i, body in enumerate(bodies):
 
         # Extract x, y values of body
@@ -108,9 +106,8 @@ def calc_orbits(bodies, n_body_func, t0, t1, dt):
         .set_integrator('dop853', rtol=1e-6, atol=1e-10) \
         .set_initial_value(initial, t0).set_f_params(bodies)
 
-    # Iterate over time intervals and integrate
+    # Iterate over time intervals and integrate, storing updated spatial coordinates and velocities of bodies
     for i in range(1, len(t)):
-        # Store updated spatial coordinates and velocities of bodies
         y[i, :] = integrator.integrate(t[i])
 
     return y
@@ -128,39 +125,39 @@ def plot_orbits(orbit_paths):
     return
 
 
+def store_orbits(bodies, orbit_paths, file_name):
+
+    # Put orbits array in pandas DataFrame
+    orbits_df = pd.DataFrame(orbit_paths)
+
+    # Add column names
+    for i in range(len(bodies)):
+        body_name = bodies[i].name
+
+        rename_spec = {
+            i * 4:     body_name + '_x',
+            i * 4 + 1: body_name + '_vx',
+            i * 4 + 2: body_name + '_y',
+            i * 4 + 3: body_name + '_vy'
+        }
+
+        orbits_df.rename(columns=rename_spec, inplace=True)
+
+    orbits_df.to_csv("{}.csv".format(file_name))
+
+
 if __name__ == '__main__':
 
     # Set up bodies
     body1 = Body(x=50, vx=0, y=0, vy=10, mass=1, name='Light 1')
     body2 = Body(x=-50, vx=0, y=0, vy=-10, mass=1, name='Light 2')
     body3 = Body(x=-0, vx=0, y=0, vy=0, mass=3000, name='Massive 1')
-
     bodies = [body1, body2, body3]
 
     print("\nCalculating orbits...")
-    orbits = calc_orbits(bodies=bodies,
-                         n_body_func=n_body_func,
-                         t0=0,
-                         t1=211,
-                         dt=1000)
+    orbits = calc_orbits(bodies=bodies, n_body_func=n_body_func, t0=0, t1=211, dt=1000)
 
     print("\nPlotting orbits...")
-    #plot_orbits(orbit_paths=orbits)
+    plot_orbits(orbit_paths=orbits)
 
-    # Put orbits array in pandas DataFrame
-    orbitsDf = pd.DataFrame(orbits)
-
-    # Add column names
-    for i in range(len(bodies)):
-
-        body_name = bodies[i].name
-
-        orbitsDf.rename(columns={i*4: body_name + '_x',
-                                i*4 + 1: body_name + '_vx',
-                                i*4 + 2: body_name + '_y',
-                                i*4 + 3: body_name + '_vy'},
-                        inplace=True)
-
-    # Let's write orbits DataFrame to a csv
-    orbitsDf.to_csv("orbits_2light_1massive.csv")
-
+    store_orbits(bodies=bodies, orbit_paths=orbits, file_name="orbits_2light_1massive")
