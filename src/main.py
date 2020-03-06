52b603ef3e3e6863957253b13233e94051b46030
Tom Boyes-Park <tombp23@googlemail.com>
2020-03-06 21:27:14 +0000

    3: removed storing of orbit data
    Also added pytest cache to the gitignore

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from src.config import DefaultConditions
-from src.orbits import calc_orbits, plot_orbits, store_orbits
+from src.orbits import calc_orbits, plot_orbits
 
 
 if __name__ == "__main__":
@@ -7,4 +7,3 @@ if __name__ == "__main__":
     orbits = calc_orbits(bodies=bodies, t0=0, t1=60, dt=5000)
 
     plot_orbits(orbit_paths=orbits)
-    store_orbits(bodies=bodies, orbit_paths=orbits, file_name="orbits_2light_1massive")
