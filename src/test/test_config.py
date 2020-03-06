3412836e47c079017963c9b08e3be8068c1b7298
Tom Boyes-Park <tombp23@googlemail.com>
2020-03-06 19:37:14 +0000

    1: separated test into 2 distinct tests

--- src/test/test_config.py
+++ src/test/test_config.py
@@ -11,7 +11,15 @@ from loaders import Body
     argnames="conditions",
     argvalues=[c.value for c in DefaultConditions],
 )
-def test_default_conditions(conditions: List[Body]):
-    assert type(conditions) == list
+def test_default_conditions_are_lists(conditions: List[Body]):
+    assert isinstance(conditions, list)
+
+
+@pytest.mark.parametrize(
+    ids=[c.name for c in DefaultConditions],
+    argnames="conditions",
+    argvalues=[c.value for c in DefaultConditions],
+)
+def test_default_conditions_are_lists_of_bodies(conditions: List[Body]):
     for body in conditions:
-        assert type(body) == Body
+        assert isinstance(body, Body)
