a1e5000a3146ccea75ddc5bee2152b11b94a0ef4
Tom Boyes-Park <tombp23@googlemail.com>
2020-04-10 17:05:09 +0100

    5: replaced print statements with proper logging

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 from src.config import DefaultConditions
 from src.orbits import calc_orbits, plot_orbits
 
+import logging
+
+
+def initialise_logger():
+    logging.basicConfig(
+        format="%(levelname)s %(asctime)s %(filename)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    logging.root.setLevel(logging.DEBUG)
+
 
 if __name__ == "__main__":
-    bodies = DefaultConditions.YING_YANG_2B
+    initialise_logger()
+
+    bodies = DefaultConditions.YING_YANG_2B.value
     orbits = calc_orbits(bodies=bodies, t0=0, t1=60, dt=5000)
 
     plot_orbits(orbit_paths=orbits)
